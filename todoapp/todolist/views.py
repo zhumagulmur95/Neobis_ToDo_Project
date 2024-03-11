9c72a51f99dc37fe299fc29bbbb6cd0a16d39379
zhumagul.mur@gmail.com <zhumagul.mur@gmail.com>
2024-03-12 01:06:47 +0600

    inition-commit

---
+++ todoapp/todolist/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+
+from .models import ToDo
+
+def index(request):
+    todos = ToDo.objects.all()
+    return render(request, 'todoapp/index.html', {'todo_list': todos, 'title': 'Главная страница'})
+
+@require_http_methods(['POST'])
+def add(request):
+    title = request.POST['title']
+    todo = ToDo(title=title)
+    todo.save()
+    return redirect('index')
+
+def update(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)
+    todo.is_complete = not todo.is_complete
+    todo.save()
+    return redirect('index')
+
+def delete(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)
+    todo.delete()
+    return redirect('index')
